4d5e5ab72a578be6bc7d3a32f9f33b0a2cbdb5ed
JHM <jj@j2.io>
2015-09-19 18:38:59 -0400

    create home/shop header files

--- src/main.c
+++ src/main.c
@@ -1,51 +1,9 @@
 #include <pebble.h>
-
-Window *my_window;
-TextLayer *text_layer;
-
-Window *main_window;
-Window *shop_main;
+#include "home.h"
+#include "shop.h"
 
 unsigned long long points;
 char *points_text;
-
-TextLayer *shop_title;
-unsigned int shop_selection;
-
-struct shop_item{
-  int id; //id of item
-  char* name;
-  unsigned long long price; //initial price
-  char increment; //increment multiplier. will be multiplied by 1+increment per item bought
-  TextLayer *text_layer;
-};
-
-struct shop_item items[3]= {
-
-    {
-        0,
-        "Zombie",
-        100,
-        2,
-        NULL
-    },
-
-    {
-        1,
-        "Child",
-        200,
-        2,
-        NULL
-    },
-
-    {
-        2,
-        "Cheetah",
-        300,
-        2,
-        NULL
-    }
-};
 
 static void tick_handler(struct tm *tick_timer, TimeUnits units_changed) { //function is called every second
   snprintf(points_text, sizeof("12345678901234567890"), "POINTS: %llu", ++points);
